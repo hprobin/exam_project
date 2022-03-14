3262987f8b7e84bf0277215af2ce0e16ceb0bca9
JeongYeonJu <yj_382@naver.com>
2022-03-14 16:16:55 +0900

    fsf

--- src/main/java/Pack01/QuestionDao.java
+++ src/main/java/Pack01/QuestionDao.java
@@ -7,35 +7,93 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
-
-
 
 public class QuestionDao {
 	@Autowired
 	ConnectionDB conn;
 	
 	QuestionDao(){}
-	List<QuestionDto> listQues() {
-		String sql = "select * from question order by rand() limit 5;";	
-		List<QuestionDto> list = new ArrayList<QuestionDto>();
+	boolean Select(TesterDto testerDto) {
+		String sql = "select number from member name = ? and number = ? and exam_ox = 'X';";
 		try {
-			Connection conn = ConnectionDB.getConnection();
-			
-			PreparedStatement pstmt = conn.prepareStatement(sql);	//5개 조회쿼리
-			ResultSet rs = pstmt.executeQuery();	//5개 조회
-			
-			while(rs.next()) {				
-				list.add(new QuestionDto(rs.getInt("index"), rs.getString("ques"), 
-						rs.getString("n1"), rs.getString("n2"), 
-						 rs.getString("n3"),  rs.getString("n4"), 
-						 rs.getInt("ans")));
+			Connection conn = ConnectionDB.getConnection();	
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, testerDto.getName());
+			pstmt.setString(2, testerDto.getNumber());
+			ResultSet rs = pstmt.executeQuery();
+			System.out.println(testerDto.getNumber());
+			while(rs.next()) {
+				if (rs.getString("number").equals(testerDto.getNumber())) {					
+					return true;
+				}
 			}
-			System.out.println(list);
-			return list;
-		}catch (Exception e) {
-			System.out.println(e.getMessage());
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
-		return null;
+		return false;
 	}
+//	boolean QuizInsert() {
+//		String sqlSelect = "select q.index from question q order by rand() limit 5;";	
+//		String sqlInsert = "insert into mAnswer values(?,?,default,?,default,?,default,?,default,?,default,default)";			
+//		List<Integer> list = new ArrayList<Integer>();
+//		try {
+//			Connection conn = ConnectionDB.getConnection();
+//			
+//			PreparedStatement pstmt = conn.prepareStatement(sqlSelect);	//5개 조회쿼리
+//			ResultSet rs = pstmt.executeQuery();	//5개 조회
+//			
+//			PreparedStatement pstmt2 = conn.prepareStatement(sqlInsert);	//5개 삽입 
+//			int rs2 = pstmt2.executeUpdate();	//5개 삽입
+//			
+//			while(rs.next()) {				
+//				list.add(rs.getInt("q.index"));
+//			}
+//			pstmt2.setInt(1, );
+//			return list;
+//		}catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		return null;
+//	}
+//	
+//	List<Integer> QuizIndex() {
+//		String sql = "select q.index from question q order by rand() limit 5;";	
+//		List<Integer> list = new ArrayList<Integer>();
+//		try {
+//			Connection conn = ConnectionDB.getConnection();
+//			PreparedStatement pstmt = conn.prepareStatement(sql);	//5개 조회쿼리
+//			ResultSet rs = pstmt.executeQuery();	//5개 조회
+//			
+//			while(rs.next()) {				
+//				list.add(rs.getInt("q.index"));
+//			}
+//			return list;
+//		}catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		return null;
+//	}
+//	
+//	boolean Insert() {
+//		
+//		return false;
+//	}
+	
+//	ResultSet Select(HttpServletRequest request) {
+//		String sql = "select * from question where index = ?;";
+//		try {
+//			Connection conn = ConnectionDB.getConnection();
+//			PreparedStatement pstmt = conn.prepareStatement(sql);
+//			
+//			pstmt.setString(1, request.getParameter("index"));
+//			ResultSet rs = pstmt.executeQuery();
+//			return rs;
+//		}catch (Exception e) {
+//			
+//		}
+//		return null;
+//	}
 }
--- src/main/webapp/WEB-INF/Views/QuestionView2.jsp
+++ src/main/webapp/WEB-INF/Views/QuestionView2.jsp
@@ -1,8 +1,6 @@
-<%@page import="org.apache.jasper.tagplugins.jstl.core.Out"%>
 <%@page import="java.util.ArrayList"%>
 <%@page import="Pack01.QuestionDto"%>
 <%@page import="java.util.List"%>
-<%@page import="java.sql.ResultSet"%>
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="EUC-KR"%>
 <!DOCTYPE html>
@@ -11,14 +9,12 @@
 <meta charset="EUC-KR">
 <title>Insert title here</title>
 </head>
-
 <body>
-<h1>문제 출제</h1>
+<h1>문제 출제2</h1>
 		<%
-		//ResultSet rs = (ResultSet) request.getAttribute("result");
-			//out.println("<td>" + quesionDto.getIndex()+"</td>");
-			List<QuestionDto> list = (List) request.getAttribute("result");
 			//QuestionDto list = (QuestionDto) request.getAttribute("result");
+			//List<QuestionDto> list = (List) request.getAttribute("result");
+			
 		%>
 		
 		<% 
@@ -27,15 +23,13 @@
 			//out.println("<input type ='radio' name = 'n' value = 2 >" + list.getN2());
 			//out.println("<input type ='radio' name = 'n' value = 3 >" + list.getN3());
 			//out.println("<input type ='radio' name = 'n' value = 4 >" + list.getN4() + "</br>");	
-			out.println("<h1>" + list.get(0).getQues()+ "</h1>");
-			out.println("<input type ='radio' name = 'n' value = 1 >" + list.get(0).getN1());
-			out.println("<input type ='radio' name = 'n' value = 2 >" + list.get(0).getN2());
-			out.println("<input type ='radio' name = 'n' value = 3 >" + list.get(0).getN3());
-			out.println("<input type ='radio' name = 'n' value = 4 >" + list.get(0).getN4() + "</br>");	
-
+			out.println("<h1>" + list.get(1).getQues()+ "</h1>");
+			out.println("<input type ='radio' name = 'n' value = 1 >" + list.get(1).getN1());
+			out.println("<input type ='radio' name = 'n' value = 2 >" + list.get(1).getN2());
+			out.println("<input type ='radio' name = 'n' value = 3 >" + list.get(1).getN3());
+			out.println("<input type ='radio' name = 'n' value = 4 >" + list.get(1).getN4() + "</br>");	
 		%>
-<button onclick='location="Question2"'>다음문제</button>
-
+			<button onclick='location="Question3"'>다음문제</button>
 
 </body>
 </html>
